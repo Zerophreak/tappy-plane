aecfdd1dcbd781becdeb6fe5f926bd6fc4d1bec7
Raymon_Rutten <80789205+Zerophreak@users.noreply.github.com>
2025-10-07 15:18:29 +0200

    Adding scene management to game

--- TappyPlane/Scenes/Game/Game.cs
+++ TappyPlane/Scenes/Game/Game.cs
@@ -3,12 +3,17 @@ using System;
 
 public partial class Game : Node2D
 {
+	private static readonly PackedScene MAIN_SCENE = GD.Load<PackedScene>("res://UI/Main/Main.tscn");
+
 	[Export] private Marker2D _spawnUpper;
 	[Export] private Marker2D _spawnLower;
 	[Export] private Node2D _pipesHolder;
 	[Export] private Timer _spawnTimer;
 	[Export] private PackedScene _pipesScene;
 	[Export] private Plane _plane;
+
+	
+	private bool _gameOver = false; 
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -23,8 +28,23 @@ public partial class Game : Node2D
     // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(double delta)
 	{
+		if(Input.IsActionJustPressed("fly") && _gameOver)
+		{
+			ChangeToMain();
+		}
 
+		if(Input.IsKeyPressed(Key.Q))
+		{
+			ChangeToMain();
+			
+		}
 	}
+
+	private void ChangeToMain()
+	{
+		GetTree().ChangeSceneToPacked(MAIN_SCENE);
+	}
+
 	public float GetSpawnY()
 	{
 		return (float)GD.RandRange(_spawnUpper.Position.Y, _spawnLower.Position.Y);
@@ -43,6 +63,7 @@ public partial class Game : Node2D
 	{
 		//GD.Print("GameOver");
 		StopPipes();
+		_gameOver = true;
 	}
 
 	private void SpawnPipes()
